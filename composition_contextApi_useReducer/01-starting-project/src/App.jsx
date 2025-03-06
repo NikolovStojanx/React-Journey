e15b5bb7d48009300210b2bf57f4a2f12e3f8c7c
Stojan <stojannikolov1@gmail.com>
2025-03-06 17:59:12 +0100

    Fashion E-Shop
    Use of composition, useRef, useReducer, Context Api

---
+++ composition_contextApi_useReducer/01-starting-project/src/App.jsx
@@ -0,0 +1,24 @@
+import { DUMMY_PRODUCTS } from './dummy-products.js';
+import Product from './components/Product.jsx';
+import Header from './components/Header.jsx';
+import Shop from './components/Shop.jsx';
+import CartContextProvider  from "./store/shopping-cart-context.jsx";
+
+// App contains all the components that will use the context
+function App() {
+
+  return (
+    <CartContextProvider>
+      <Header />
+      <Shop>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} />
+          </li>
+        ))}
+      </Shop>
+    </CartContextProvider>
+  );
+}
+
+export default App;
